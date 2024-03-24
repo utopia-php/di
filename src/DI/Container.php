0ee625032f4add14191e5446f4588efe53004c01
Eldad Fux <eldad.fux@gmail.com>
2024-03-24 21:55:19 +0100

    POC

--- src/DI/Container.php
+++ src/DI/Container.php
@@ -4,12 +4,12 @@ namespace Utopia\DI;
 
 use Exception;
 
-class DI
+class Container
 {
     /**
      * @var array
      */
-    protected static array $callbacks = [];
+    protected array $dependencies = [];
 
     /**
      * @var array
@@ -17,94 +17,51 @@ class DI
     protected array $resources = [];
 
     /**
-     * Set a new resource callback
+     * TBD
      *
-     * @param  string  $name
-     * @param  callable  $callback
-     * @param  array  $injections
+     * @param  Dependency  $dependency
      * @return void
      *
      * @throws Exception
      */
-    public static function setResource(string $name, callable $callback, array $injections = []): void
+    public function set(Dependency $dependency): void
     {
-        if ($name === 'utopia') {
+        if ($dependency->getName() === 'utopia') {
             throw new Exception("'utopia' is a reserved keyword.", 500);
         }
 
-        self::$callbacks[$name] = ['callback' => $callback, 'injections' => $injections, 'resets' => []];
+        $this->dependencies[$dependency->getName()] = $dependency;
     }
 
     /**
-     * If a resource has been created return it, otherwise create it and then return it
-     *
-     * @param  string  $name
-     * @param  bool  $fresh
-     * @return mixed
-     *
-     * @throws Exception
-     */
-    public function getResource(string $name, bool $fresh = false): mixed
-    {
-        if ($name === 'utopia') {
-            return $this;
-        }
-
-        if (!\array_key_exists($name, $this->resources) || $fresh || (self::$callbacks[$name]['reset'] ?? true)) {
-            if (!\array_key_exists($name, self::$callbacks)) {
-                throw new Exception('Failed to find resource: "' . $name . '"');
-            }
-
-            $this->resources[$name] = \call_user_func_array(
-                self::$callbacks[$name]['callback'],
-                $this->getResources(self::$callbacks[$name]['injections'])
-            );
-        }
-
-        self::$callbacks[$name]['reset'] = false;
-
-        return $this->resources[$name];
-    }
-
-    /**
-     * Get Resources By List
+     * TBD
      *
      * @param  array  $list
      * @return array
      */
-    public function getResources(array $list): array
-    {
-        $resources = [];
-
-        foreach ($list as $name) {
-            $resources[$name] = $this->getResource($name);
-        }
-
-        return $resources;
-    }
-
-    /**
-     * Get Resources By List
-     *
-     * @param  array  $list
-     * @return array
-     */
-    public function inject(Hook $hook): mixed
+    public function get(Injection $injection): mixed
     {
         $arguments = [];
 
-        foreach ($hook->getParams() as $key => $param) {
-            $value = $this->getResource($param['name']);
-            $hook->setParamValue($key, $value);
-            $arguments[$param['order']] = $value;
+        foreach ($injection->getDependencies() as $dependency) {
+            if ($dependency->getName() === 'utopia') {
+                return $this;
+            }
+    
+            if (!\array_key_exists($dependency->getName(), $this->resources)) {
+                if (!\array_key_exists($dependency->getName(), $this->dependencies)) {
+                    throw new Exception('Failed to find resource: "' . $dependency->getName() . '"');
+                }
+    
+                $this->resources[$dependency->getName()] = \call_user_func_array(
+                    $this->dependencies[$dependency->getName()]->getCallback(),
+                    $this->get($this->dependencies[$dependency->getName()]->getDependencies())
+                );
+            }
+    
+            $arguments[] = $this->get($dependency->getName());
         }
 
-        foreach ($hook->getInjections() as $key => $injection) {
-            $arguments[$injection['order']] = $this->getResource($injection['name']);
-        }
-
-        \call_user_func_array($hook->getAction(), $arguments);
+        \call_user_func_array($injection->getCallback(), $arguments);
     }
-
-
 }
