053ebd097963190c5b279d66c5b530acb584d8c0
Luke B. Silver <22452787+loks0n@users.noreply.github.com>
2026-06-26 11:48:18 +0100

    Merge pull request #40 from utopia-php/feat/rector-hoist
    build: hoist baseline rector + phpstan configs to the root

--- rector.php
+++ rector.php
@@ -3,21 +3,7 @@
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector;
-use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ])
-    ->withSkip([
-        __DIR__ . '/vendor',
-        ThrowWithPreviousExceptionRector::class,
-    ])
-    ->withPhpSets(php82: true)
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
-        phpunitCodeQuality: true,
-    );
+return (require __DIR__ . '/../../rector.php')->withSkip([
+    ThrowWithPreviousExceptionRector::class,
+]);
